9f9c7465594b786cb3c7fc84cb5d2a975a09c66b
Yonky <123@qq.com>
2024-10-11 15:53:11 +0800

    message_data

---
+++ process.py
@@ -0,0 +1,21 @@
+import json
+
+all_lans = {'cs': 'Czech', 'de': 'German', 'ru': 'Russian', 'zh': 'Chinese', 'en': 'English'}
+
+def make_jsonl(src, tgt):
+    src_file_path = f"./raw_data/test.{src}-{tgt}.{src}"
+    output_file_path = f"./message_data/{src}-{tgt}.{src}.jsonl"
+    with open(src_file_path, 'r', encoding='utf-8') as src_file, open(output_file_path, 'w', encoding='utf-8') as jsonl_file:
+        for line in src_file:
+            src_sentence = line.strip()  
+            entry = {
+                "role": "user",
+                "content": f"Translate: This is an {all_lans[src]} to {all_lans[tgt]} translation, please provide the {all_lans[tgt]} translation for this sentence. Do not provide any explanations or text apart from the translation. \n{all_lans[src]}: {src_sentence} \n{all_lans[tgt]}: "
+            }
+            jsonl_file.write(json.dumps(entry, ensure_ascii=False) + '\n')
+
+lans = ['cs', 'de', 'ru', 'zh']
+for lan in lans:
+    for src in [lan, 'en']:
+        tgt = 'en' if src == lan else lan
+        make_jsonl(src, tgt)
